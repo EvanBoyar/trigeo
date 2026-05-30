560611c2545c4bb277fdead6e8ba4cba16fe9ec1
EvanBoyar <evanboyar@gmail.com>
2026-05-30 14:16:08 -0400

    Bump to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.trigeo.app"
         minSdk = 31
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.2.0"
+        versionCode = 11
+        versionName = "0.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
